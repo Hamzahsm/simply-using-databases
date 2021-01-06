93931c94f5537c071598d24accd53d13f333d39b
Hamzahsm <hamzahsaifulmadjid@gmail.com>
2021-01-06 12:07:36 +0700

    ada php file

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>simpy-databases</title>
+</head>
+<body>
+    <h1>Your Data is here</h1>
+</body>
+</html>
